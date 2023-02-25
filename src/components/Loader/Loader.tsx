d9d26c4581cb9fc404721958aa86942f513b40e1
Nick <escoffier.n@gmail.com>
2023-02-25 11:52:49 -0800

    Add Loader (component) + mixins (SCSS) folder

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import cn from 'classnames';
+import { LoaderProps } from './Loader.types';
+import styles from './Loader.module.scss';
+
+export const Loader: FC<LoaderProps> = ({ className, ...props }) => {
+  return (
+    <div className={cn(styles.root, className)} {...props}>
+      <div className={styles.loader} />
+    </div>
+  );
+};
